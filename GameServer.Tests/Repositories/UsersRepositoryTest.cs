7436e983e455c156b579788c8b6be68c8821e01f
Yoan THIRION <ythirion@agilepartner.net>
2017-10-13 17:19:38 +0200

    Fix build

--- GameServer.Tests/Repositories/UsersRepositoryTest.cs
+++ GameServer.Tests/Repositories/UsersRepositoryTest.cs
@@ -11,15 +11,6 @@ namespace SandTigerShark.GameServer.Tests
         [InlineData("User1")]
         [InlineData("User2")]
         [InlineData("UnkonwnUser")]
-        public void UserRepositoryCannotReturnNullToken(string userName)
-        {
-            Assert.NotNull(new UserRepository().GetUserToken(userName));
-        }
-
-        [Theory]
-        [InlineData("User1")]
-        [InlineData("User2")]
-        [InlineData("UnkonwnUser")]
         public void UserRepositoryReturnTheSameTokenForAGivenUser(string userName)
         {
             Assert.Equal(
