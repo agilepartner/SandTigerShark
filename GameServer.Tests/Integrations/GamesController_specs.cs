099a5ad2466e26b3ebbc7e1a76a36718963761b0
jrousset <jrousset@agilepartner.net>
2017-10-13 18:01:28 +0200

    - Introduced tests for GamesController

---
+++ GameServer.Tests/Integrations/GamesController_specs.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace SandTigerShark.GameServer.Tests.Integrations
+{
+    public class GamesController_specs
+    {
+        public class when_I_request_a_game_status_that_does_not_exists : WebApiTest<Startup>
+        {
+
+            [Fact]
+            public async void then_I_get_a_not_found_status_code()
+            {
+                using (var response = await Client.GetAsync("api/games/tarteenpion"))
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+                }
+
+            }
+        }
+
+    }
+}
