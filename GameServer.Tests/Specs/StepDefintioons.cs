0930cf83eb20a4be59cdc8e1133631b1e83668b8
Yoan THIRION <ythirion@agilepartner.net>
2017-10-16 15:20:42 +0200

    Add Specflow to dotnet core project

---
+++ GameServer.Tests/Specs/StepDefintioons.cs
@@ -0,0 +1,43 @@
+﻿namespace SandTigerShark.GameServer.Tests.Specs
+{
+    using TechTalk.SpecFlow;
+
+    namespace Sample.Website.Tests
+    {
+        [Binding]
+        public class StepDefinitions
+        {
+            [Given(@"I am curious")]
+            public void GivenIAmCurious()
+            {
+
+            }
+
+            [When(@"I request the version")]
+            public void WhenIRequestTheVersion()
+            {
+
+            }
+
+            [When(@"I yell '(.*)'")]
+            public void WhenIYell(string exclamation)
+            {
+            }
+
+            [Then(@"the result is content")]
+            public void ThenTheResultIsContent()
+            {
+            }
+
+            [Then(@"the result is constant")]
+            public void ThenTheResultIsConstant()
+            {
+            }
+
+            [Then(@"I hear '(.*)' echoed back")]
+            public void ThenIHearEchoedBack(string exclamation)
+            {
+            }
+        }
+    }
+}
